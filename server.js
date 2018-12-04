b50f81917f079a9c31cd1d728f5e3218d182cb70
MooHamOinkBeef <imtheward@gmail.com>
2018-12-03 17:35:06 -0800

    Added basic server functions so you can get the various webpages from the server, will convert for template usage later.

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3034;
 
 app.use(express.static('public'));
+
+app.get('/', function (req, res, next) {
+   res.status(200).sendFile(__dirname + '/public/Index.html');
+});
+
+app.get('*', function (req, res, next) {
+   res.status(404).sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(port, function () {
    console.log("== Server is listening on port", port);
