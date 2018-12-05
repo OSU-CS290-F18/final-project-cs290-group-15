a4a04d87495a7c54e347025a10d6ebad249de82b
Chris Lasneski <lasneskc@oregonstate.edu>
2018-12-04 20:05:43 -0800

    server.js without any mongo

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 
 var tableData = require('./tableData');
-
-var mongoHost = process.env.MONGO_HOST;
-var mongoPort = process.env.MONGO_PORT || '27017';
-var mongoUsername = process.env.MONGO_USERNAME;
-var mongoPassword = process.env.MONGO_PASSWORD;
-var mongoDBName = process.env.MONGO_DB_NAME;
-
-var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
-
-var mongoDB = null;
+//
+//var mongoHost = process.env.MONGO_HOST;
+//var mongoPort = process.env.MONGO_PORT || '27017';
+//var mongoUsername = process.env.MONGO_USERNAME;
+//var mongoPassword = process.env.MONGO_PASSWORD;
+//var mongoDBName = process.env.MONGO_DB_NAME;
+//
+//var mongoURL = "mongodb://" + mongoUsername + ":" + mongoPassword + "@" + mongoHost + ":" + mongoPort + "/" + mongoDBName;
+//
+//var mongoDB = null;
 
 var app = express();
 
@@ -28,7 +28,7 @@ var port = process.env.PORT || 3034;
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(bodyParser.json());
+//app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
@@ -52,11 +52,11 @@ app.get('/products/:product', function (req, res, next) {
 app.get('*', function (req, res, next) {
    res.status(404).render('404', {});
 });
-MongoClient.connect(mongoURL, function (err, client) {
-    if (err){
-        throw err;
-    }
-    mongoDB = client.db(mongoDBName);
+//MongoClient.connect(mongoURL, function (err, client) {
+//    if (err){
+//        throw err;
+//    }
+//    mongoDB = client.db(mongoDBName);
     app.listen(port, function () {
         console.log("== Server is listening on port", port);
 });
